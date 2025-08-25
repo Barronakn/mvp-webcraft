872bd3ec3446caf3d400ab0bf370b86d16ccaf70
Barronakn <barronakn@gmail.com>
2025-08-25 16:11:33 +0100

    `Updated dependencies and package.json to reflect changes in the codebase`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,23 @@
-import type { Metadata } from "next";
-import { Lato, Playfair_Display } from "next/font/google";
-import "./globals.css";
-import Providers from "./providers";
-
+import type { Metadata } from 'next';
+import { Lato, Playfair_Display } from 'next/font/google';
+import './globals.css';
+import Providers from './providers';
+import { ConvexClientProvider } from './ConvexClientProvider copy';
 
 const lato = Lato({
-  variable: "--font-lato",
-  subsets: ["latin"],
-  weight: ["100", "300", "400", "700", "900"],
+  variable: '--font-lato',
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '700', '900'],
 });
 
-
 const playfairDisplay = Playfair_Display({
-  variable: "--font-playfairDisplay",
-  subsets: ["latin"],
+  variable: '--font-playfairDisplay',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Wakpon",
-  description: "Plateforme Tourisme & Artisanat Béninois",
+  title: 'Wakpon',
+  description: 'Plateforme Tourisme & Artisanat Béninois',
 };
 
 export default function RootLayout({
@@ -28,13 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
-      <Providers>
       <body
         className={`${lato.variable} ${playfairDisplay.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          <ConvexClientProvider>{children}</ConvexClientProvider>
+        </Providers>
       </body>
-      </Providers>
     </html>
   );
 }
