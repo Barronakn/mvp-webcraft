aec766530a85ed28d57e6b5e0a27f2ba1242ec9c
Barronakn <barronakn@gmail.com>
2025-09-06 19:52:21 +0100

    `Refactor Navbar component to use usePathname hook and dynamic links`

--- app/(home)/_components/header/Navbar.tsx
+++ app/(home)/_components/header/Navbar.tsx
@@ -3,32 +3,41 @@
 import Logo from '@/components/logo';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const Navbar = () => {
   const router = useRouter();
+  const pathname = usePathname();
+
+  const links = [
+    { href: '/', label: 'Accueil' },
+    { href: '/about', label: 'À Propos de nous' },
+    { href: '/sites', label: 'Sites Touristiques' },
+    { href: '/contact', label: 'Contactez-nous' },
+  ];
 
   return (
     <nav className="flex px-6 lg:px-16 z-50 bg-cream-300 fixed left-0 top-0 w-full py-4 lg:py-4 justify-between items-center shadow">
       <Logo />
-      <div className="hidden lg:flex flex-row text-gra space-x-20 items-center">
-        <div className="flex flex-row space-x-4 items-center">
-          <Link href="/" className="text-gray-800 hover:text-terracotta-500 text-base font-inter font-semibold transition duration-300">
-            Accueil
-          </Link>
-          <Link href="/about" className="text-gray-800 hover:text-terracotta-500 text-base font-inter font-semibold transition duration-300">
-            À Propos de nous
-          </Link>
-          <Link
-            href="/sites"
-            className="text-gray-800 hover:text-terracotta-500 text-base font-inter font-semibold transition duration-300"
-          >
-            Sites Touristiques
-          </Link>
-          <Link href="/contact" className="text-gray-800 hover:text-terracotta-500 text-base font-inter font-semibold transition duration-300">
-            Contactez-nous
-          </Link>
+      <div className="hidden lg:flex flex-row space-x-12 items-center">
+        <div className="flex flex-row space-x-8 items-center">
+          {links.map((link) => {
+            const isActive = pathname === link.href;
+            return (
+              <Link
+                key={link.href}
+                href={link.href}
+                className={`text-base font-inter font-semibold transition duration-300 ${
+                  isActive
+                    ? 'bg-clip-text text-transparent bg-linear-65 from-terracotta-500 to-sand-500'
+                    : 'text-gray-800 hover:text-terracotta-500'
+                }`}
+              >
+                {link.label}
+              </Link>
+            );
+          })}
         </div>
 
         <Button
