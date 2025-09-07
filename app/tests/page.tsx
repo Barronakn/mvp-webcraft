5520963b829e2ee996e96870573388c0a462ab4e
Hervé Zossou <hervezossou8@gmail.com>
2025-09-07 04:17:55 +0100

    feat(tests): nested all test components in the page

--- app/tests/page.tsx
+++ app/tests/page.tsx
@@ -1,7 +1,11 @@
 import TestGetSite from "@/components/tests/site-display";
+import SitesList from "../../components/tests/sites-list";
 
 export default function Page() {
     return (
-        <TestGetSite />
+        <main className="mx-auto">
+            <TestGetSite />
+            <SitesList />
+        </main>
     )
 }
